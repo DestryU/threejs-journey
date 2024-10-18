189b9d7e1f55cace977c118d68a769132670abd5
DestryU <47061782+DestryU@users.noreply.github.com>
2024-10-17 18:11:21 -0600

    Finished lesson 09

--- Lessons/les-09/src/script.js
+++ Lessons/les-09/src/script.js
@@ -1,6 +1,28 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from 'lil-gui'
+
+/*
+Debug object
+*/
+const gui = new GUI({
+    width: 300,
+    title: "Debugger",
+    closeFolders: true
+})
+gui.close()
+gui.hide()
+const globalDebug = {}
+
+window.addEventListener('keydown', (event) => {
+    if(event.key == '`') {
+        gui.show(gui._hidden)
+    }
+})
+
+
+
 
 /**
  * Base
@@ -14,10 +36,68 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+
+globalDebug.color = '#ff0000'
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: globalDebug.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+const cubeTweaks = gui.addFolder('Cube Tweaks')
+const colorTweak = cubeTweaks.addFolder('Color Tweak')
+
+cubeTweaks
+    .add(mesh.position, 'y')
+    .min(-2)
+    .max(2)
+    .step(.01)
+    .name('Height')
+
+const myObj = {
+    myVariable: 1337
+}
+
+cubeTweaks
+    .add(mesh, 'visible')
+    .name('Show Visible')
+
+cubeTweaks
+    .add(material, 'wireframe')
+    .name('Show Wireframe')
+
+
+colorTweak
+    .addColor(globalDebug, 'color')
+    .onChange(() => {
+        material.color.set(globalDebug.color)
+    })
+    .name('Box Color')
+
+globalDebug.boxSpin = () => {
+    gsap.to(mesh.rotation, {y: mesh.rotation.y + Math.PI * 2})
+}
+
+cubeTweaks
+    .add(globalDebug, 'boxSpin')
+    .name('Give the box a spin!')
+
+globalDebug.subdiv = 2
+cubeTweaks
+    .add(globalDebug, 'subdiv')
+    .min(1)
+    .max(20)
+    .step(1)
+    .onFinishChange(() => {
+        mesh.geometry.dispose()
+        mesh.geometry = new THREE.BoxGeometry(1, 1, 1, globalDebug.subdiv, globalDebug.subdiv, globalDebug.subdiv)
+    })
+    .name('Sub-Division Levels')
+
+
+
+
+
 
 /**
  * Sizes
