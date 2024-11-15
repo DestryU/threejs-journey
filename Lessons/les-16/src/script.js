fe4ebaed49fe4b8c777715e87fbdd43fdc9f7f19
DestryU <47061782+DestryU@users.noreply.github.com>
2024-11-15 15:32:06 -0700

    Created and update the floor; set scale for scene

--- Lessons/les-16/src/script.js
+++ Lessons/les-16/src/script.js
@@ -25,6 +25,17 @@ const sphere = new THREE.Mesh(
 )
 scene.add(sphere)
 
+
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshStandardMaterial()
+)
+
+floor.rotation.x = Math.PI * -0.5
+
+scene.add(floor)
+
+
 /**
  * Lights
  */
