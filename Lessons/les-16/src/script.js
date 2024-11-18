9a4dd86e61c3f87e51ab16196d8a1053692e2e3e
DestryU <47061782+DestryU@users.noreply.github.com>
2024-11-18 14:14:52 -0700

    Adjusted light values; added light above door frame.

--- Lessons/les-16/src/script.js
+++ Lessons/les-16/src/script.js
@@ -256,19 +256,28 @@ for(let i = 0; i < 30; i++) {
     graves.add(grave)
 }
 
+/* 
 
+    Lights
 
-/**
- * Lights
- */
-// Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+*/
+
+// Ambient light //
+
+const ambientLight = new THREE.AmbientLight('#86cdff', 0.2)
 scene.add(ambientLight)
 
-// Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+// Directional light //
+
+const directionalLight = new THREE.DirectionalLight('#86cdff', 1)
 directionalLight.position.set(3, 2, -8)
 scene.add(directionalLight)
+
+const doorlight = new THREE.PointLight('#ff7d46', 5)
+doorlight.position.set(0, 2.1, 2.5)
+scene.add(doorlight)
+
+
 
 /**
  * Sizes
